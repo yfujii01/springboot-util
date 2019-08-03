b4025269577f7b9915fde07b466ede8f06a8111e
yfujii01 <yfujii01@gmail.com>
2019-08-03 22:51:46 +0900

    add requests.http

--- script.bash
+++ script.bash
@@ -6,17 +6,19 @@ function setting(){ # settingファイルを独自のものに置き換える
     curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/script.bash
     
     # .gitignore
-    curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/.gitignore
+    curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/setting/.gitignore
     
     # application.properties
-    curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/application.properties
+    curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/setting/application.properties
     mv application.properties ./src/main/resources/application.properties
 
     # flyway用マイグレーションSQL
-    curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/V1__Initail_DB.sql
+    curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/setting/V1__Initail_DB.sql
     mkdir -p ./src/main/resources/db/migration
     mv V1__Initail_DB.sql ./src/main/resources/db/migration/V1__Initail_DB.sql
 
+    # requests.http
+    curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/setting/requests.http
 }
 
 # 引数に指定した関数を実行する
