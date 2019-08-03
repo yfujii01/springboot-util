3f3c137214c1a50d6f664d76a4283c3a7e898fd3
yfujii01 <33822726+yfujii01@users.noreply.github.com>
2019-08-03 20:10:24 +0900

    Create script.bash

---
+++ script.bash
@@ -0,0 +1,22 @@
+function help(){ # コマンド一覧を表示する
+    cat ./script.bash | grep "^function [A-z]"
+}
+function setting(){ # settingファイルを独自のものに置き換える
+    # スクリプトダウンロード
+    curl -L -O https://raw.githubusercontent.com/yfujii01/springboot-util/master/script.bash
+}
+
+# 引数に指定した関数を実行する
+for x in "$@"
+do
+    set -x
+    $x
+    set +x
+done
+
+# 引数が指定されない場合はhelpを実行する
+if [ -z "$@" ]; then
+    set -x
+    help
+    set +x
+fi
