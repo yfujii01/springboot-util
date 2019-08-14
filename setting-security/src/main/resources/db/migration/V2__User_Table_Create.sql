fc361fe0198ebee93b12915013d493d2932b99c0
yfujii01 <yfujii01@gmail.com>
2019-08-14 16:05:37 +0900

    fix security migrate sqlがh2db以外で動かないDDLだったため修正

--- setting-security/src/main/resources/db/migration/V2__User_Table_Create.sql
+++ setting-security/src/main/resources/db/migration/V2__User_Table_Create.sql
@@ -2,14 +2,14 @@
 -- https://docs.spring.io/spring-security/site/docs/3.0.x/reference/appendix-schema.html
 
 create table users(
-  username varchar_ignorecase(255) not null primary key,
-  password varchar_ignorecase(255) not null,
+  username varchar(255) not null primary key,
+  password varchar(255) not null,
   enabled boolean not null
 );
 
 create table authorities (
-  username varchar_ignorecase(255) not null,
-  authority varchar_ignorecase(255) not null,
+  username varchar(255) not null,
+  authority varchar(255) not null,
   constraint fk_authorities_users foreign key(username) references users(username) on delete cascade
 );
 
