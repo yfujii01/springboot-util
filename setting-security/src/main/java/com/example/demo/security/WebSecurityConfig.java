18d3082ab91a1afe3a1cc9cb7fbdae48a983fccc
yfujii01 <yfujii01@gmail.com>
2019-08-16 10:19:26 +0900

    add securityにcors設定を追加

--- setting-security/src/main/java/com/example/demo/security/WebSecurityConfig.java
+++ setting-security/src/main/java/com/example/demo/security/WebSecurityConfig.java
@@ -1,5 +1,9 @@
 package com.example.demo.security;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -8,6 +12,11 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.Data;
 
 /** 認証・認可設定 */
 @Configuration
@@ -25,36 +34,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {
     http
         // CORS
-        .cors()
+        .cors().configurationSource(corsConfigurationSource())
         // 認証設定
-        .and()
-        .authorizeRequests()
+        .and().authorizeRequests()
         // 認証無しでアクセス許可するURL
-        .antMatchers("/login", "/public")
-        .permitAll()
+        .antMatchers("/login", "/public").permitAll()
         // ADMINのみがアクセス可能
-        .antMatchers("/adminonly", "/users/**")
-        .hasAuthority("ADMIN")
+        .antMatchers("/adminonly", "/users/**").hasAuthority("ADMIN")
         // USERのみがアクセス可能
-        .antMatchers("/useronly")
-        .hasAuthority("USER")
+        .antMatchers("/useronly").hasAuthority("USER")
         // ADMINとUSERがアクセス可能
-        .antMatchers("/userandadminonly")
-        .hasAnyAuthority("ADMIN", "USER")
+        .antMatchers("/userandadminonly").hasAnyAuthority("ADMIN", "USER")
         // その他のURLは全て認証を必要とさせる
-        .anyRequest()
-        .authenticated()
+        .anyRequest().authenticated()
         // CSRF
-        .and()
-        .csrf()
-        .disable()
+        .and().csrf().disable()
         // カスタム認証用のフィルターを追加
         .addFilter(new CustomAuthenticationFilter(authenticationManager()))
         // カスタム認可用のフィルターを追加
         .addFilter(new CustomAuthorizationFilter(authenticationManager()))
         // HttpSessionを使用しない
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
   }
 
   /** 認証プロバイダのコア機能を実装 */
@@ -62,4 +62,48 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   public AuthenticationProvider getAuthenticationProvider() {
     return new CustomUserDetailsAuthenticationProvider();
   }
+
+  /**
+   * CORS設定
+   *
+   * @return CORS設定
+   */
+  private CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+    corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
+
+    // この設定が無いとフロント側でAuthorizationにアクセスできない
+    // http://hnak.hatenablog.jp/entry/2018/07/08/125216
+    corsConfiguration.addExposedHeader("Authorization");
+
+    // 環境変数からCORSを許可するURLを設定する
+    // corsConfiguration.addAllowedOrigin(System.getenv("FRONT_URL"));
+    
+    // プロパティファイルからCORSを許可するURLを設定する
+    param().getAllowedOrigins().forEach(p->{
+      corsConfiguration.addAllowedOrigin(p);
+    });
+
+    corsConfiguration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource corsConfigurationSource = new UrlBasedCorsConfigurationSource();
+    corsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+
+    return corsConfigurationSource;
+  }
+
+  /** プロパティファイルから設定を読み込むクラス */
+  @ConfigurationProperties(prefix = "custom.security.param")
+  @Data
+  private class SecurityPropertiesParameter {
+    private List<String> allowedOrigins = new ArrayList<>();
+  }
+
+  /** プロパティファイル設定 */
+  @Bean
+  public SecurityPropertiesParameter param() {
+    return new SecurityPropertiesParameter();
+  }
+
 }
